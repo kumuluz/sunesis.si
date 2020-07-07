c7598a1f64b41dd70a47a1a4c27839bbbd803eba
Miha Jamsek <miha_jamsek@windowslive.com>
2020-07-07 19:03:58 +0200

    added process optimization page

---
+++ src/pages/projects/optimizacija-procesov/index.js
@@ -0,0 +1,139 @@
+import React, {Component} from "react";
+import * as PropTypes from 'prop-types';
+import Helmet from "react-helmet";
+import {Col, Container, Row} from "reactstrap";
+
+import {GoogleAnalyticsService} from "../../../components/google-analytics/google-analytics.service";
+import {Footer} from "../../../components";
+
+import "./index.scss";
+import euLogo from "../../../assets/images/projects/eu_logo_cut.jpg";
+import ministryLogo from "../../../assets/images/projects/ministry_logo_cut.png";
+import spiritSloveniaLogo from "../../../assets/images/projects/spirit_slo.jpg";
+import {Layout} from "../../../layouts";
+
+export default class ProcessOptimizationPage extends Component {
+    
+    static propTypes = {
+        pageContext: PropTypes.string
+    };
+    
+    componentDidMount() {
+        GoogleAnalyticsService.registerPageView();
+    }
+    
+    render() {
+        const {pageContext} = this.props;
+        const locale = pageContext.locale;
+        return (
+            <Layout locale={"en"}>
+                <div className="projects-page">
+                    <Helmet title="Optimizacija procesov"/>
+                    
+                    <Container fluid={true} className="banner-dark pl-0 pr-0">
+                        <Row className="justify-content-center m-0">
+                            <Col xs="12">
+                                <h1 className="text-center">Optimizacija procesov podjetja Sunesis (OP-S)</h1>
+                            </Col>
+                        </Row>
+                        <div className="arrow"/>
+                    </Container>
+                    
+                    <Container fluid={true} className="process-opt-logos">
+                        <Row className="justify-content-center">
+                            <Col sm={12} md={4} lg={2} className="sponsor-logo">
+                                <img src={euLogo} alt="EU"/>
+                            </Col>
+                            <Col sm={12} md={4} lg={2} className="sponsor-logo">
+                                <img src={spiritSloveniaLogo} alt="Spirit Slovenia"/>
+                            </Col>
+                            <Col sm={12} md={4} lg={2} className="sponsor-logo">
+                                <img src={ministryLogo} alt="RS - Ministry for economical development and technology"/>
+                            </Col>
+                        </Row>
+                        <Row className="justify-content-center mt-5">
+                            <Col sm={7}>
+                                <ul className="process-opt-ul">
+                                    <li>Naložbo sofinancirata Republika Slovenija in Evropska unija iz Evropskega sklada za regionalni razvoj.</li>
+                                    <li>
+                                        Spletna stran EKP v Sloveniji:
+                                        &nbsp;
+                                        <a className="skladi-url" href="http://www.eu-skladi.si" target="_blank"
+                                            rel="noreferrer noopener">
+                                            www.eu-skladi.si
+                                        </a>
+                                    </li>
+                                    <li>Višina sofinanciranja: 20.000 EUR</li>
+                                </ul>
+                            </Col>
+                        </Row>
+                    </Container>
+                    
+                    <Container fluid={true} className="project-header">
+                        <Row className="justify-content-center mt-5">
+                            <Col xs={12} md={10} lg={8}>
+                                <h2>Optimizacija procesov podjetja Sunesis (OP-S)</h2>
+                                
+                                <p>
+                                    Operacija OP-S zajema celostno prenovo poslovnih procesov in drugih organizacijskih
+                                    prvin (organizacijska shema ipd.), ki bodo prispevali k prihranku časa in
+                                    večji produktivnosti podjetja, standardizaciji poslovanja in doseganju kakovosti
+                                    ter doseganju večje dodane vrednosti. Celostna prenova bo prispevala k izboljšanju
+                                    ključnih indikatorjev poslovanja podjetja, doseženo pa bo tudi aktivnejše širjenje
+                                    na tuje trge.  Celostna prenova poslovnih procesov bo pokrivala naslednje
+                                    ključne poslovne funkcije:
+                                </p>
+                                
+                                <h3>Marketing</h3>
+                                <p>
+                                    Prenova marketinški poslovnih procesov bo zajemala tako uvedbo povsem novih
+                                    procesov za profesionalno vzpostavitev marketinga kot poslovne funkcije,
+                                    s primarnim ciljem generiranja novih ”leadov”, obstoječi marketinški procesi
+                                    pa bodo ustrezno posodobljeni.
+                                </p>
+                                
+                                <h3>Prodaja</h3>
+                                <p>
+                                    Prenova prodajnih procesov bo zajemala celotno profesionalno vzpostavitev prodaje,
+                                    ki bo temeljila na prodajnem lijaku. Na podlagi prodajnega lijaka bo prenova
+                                    zajemala ustrezno identifikacijo potencialnih kupcev ter nato vodenje kupcev skozi
+                                    različne faze prodajnega procesa.
+                                </p>
+                                
+                                <h3>Kadri</h3>
+                                <p>
+                                    Kadrovski procesi so ključni v IT panogi, saj je povpraševanje po
+                                    tehničnih kadrih bistveno večje od ponudbe. Posodobitev kadrovskih procesov
+                                    se bo dotikalo tako vzpostavitve procesov za pridobivanje novega kadra, kot tudi
+                                    za ”on-boarding” proces ter razvoj in vodenja kadrov.
+                                </p>
+                                
+                                <h3>Uvedba vitkega poslovanja</h3>
+                                <p>
+                                    Vitko poslovanje je ključno pri tem, da se podjetje osredotoči na tiste aktivnosti,
+                                    ki prinašajo največjo dodano vrednost s čim manj ”odpada” in izpada produktivnosti.
+                                    To zajema predvsem iskanje ”produkt-trg” preseka, kjer je največja dodana vrednost
+                                    in možnost za rast, obenem pa uvedba procesov ter standardov, ki omogočajo produktivno
+                                    delo, torej doseči čim več s čim manjšo uporabo virov.
+                                </p>
+                                
+                                <p className="process-opt-goals">
+                                    Cilj operacije OP-S je doseganje petih ključnih ciljev:
+                                    <ul className="process-opt-ul">
+                                        <li>Povečanje prodaje</li>
+                                        <li>Povečanje produktivnosti in dodane vrednosti</li>
+                                        <li>Pridobivanje, uvedba in razvoj kadrov</li>
+                                        <li>Razvoj novi proizvodov, trgov in projektov na podlagi vitke metodologije</li>
+                                        <li>Uvedba Kaizen kulture podjetja</li>
+                                    </ul>
+                                </p>
+                            </Col>
+                        </Row>
+                    </Container>
+                    
+                    <Footer social locale={locale}/>
+                </div>
+            </Layout>
+        );
+    }
+}
