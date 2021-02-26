3882f4bf3b36e2a65dac67b19629a608be770d36
Miha Jamsek <miha_jamsek@windowslive.com>
2021-02-26 13:14:39 +0100

    added project

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -11,6 +11,8 @@ import euLogo from "../../assets/images/projects/eu_logo.jpg";
 import ministryLogo from "../../assets/images/projects/ministry_logo.png";
 import crowdsensingLogo from "../../assets/images/projects/crowdsensing_logo.png";
 import uhubLogo from "../../assets/images/projects/uhub_logo.png";
+import apiSponsorLogo from "../../assets/images/projects/sponsor_logo_1.png";
+import apiLogo from "../../assets/images/projects/api_logo – kopija.svg";
 import {Layout} from "../../layouts";
 
 export default class ProjectsPage extends Component {
@@ -39,17 +41,76 @@ export default class ProjectsPage extends Component {
                         </Row>
                         <div className="arrow"/>
                     </Container>
-
+    
+    
+    
+    
                     <Container fluid={true}>
                         <Row className="justify-content-center">
-                            <Col sm={12} md={6} lg={3} className="sponsor-logo">
+                            <Col sm={12} md={4} lg={2} className="sponsor-logo">
                                 <img src={euLogo} alt="EU"/>
                             </Col>
-                            <Col sm={12} md={6} lg={3} className="sponsor-logo">
+                            <Col sm={12} md={4} lg={2} className="sponsor-logo sponsor-logo-1">
+                                <img src={apiSponsorLogo} alt=""/>
+                            </Col>
+                            <Col sm={12} md={4} lg={2} className="sponsor-logo">
                                 <img src={ministryLogo} alt="RS - Ministry for economical development and technology"/>
                             </Col>
                         </Row>
                     </Container>
+    
+                    <Container fluid={true} className="project-header">
+                        <Row className="align-items-center flex-column">
+                            <Col xs={12} className=" d-flex flex-column align-items-center">
+                                <h2 className="text-center api-header">
+                                    Kumuluz
+                                    <span className="api-emph">API</span>
+                                </h2>
+                                <h3>
+                                    Platforma KumuluzAPI za upravljanje ekosistema APIjev v okoljih pametnih mest in skupnosti
+                                </h3>
+                            </Col>
+                        </Row>
+                        <Row className="justify-content-center mt-5">
+                            <Col xs={12} md={10} lg={8}>
+                                <p>
+                                    Cilj projekta je razviti inovativno platformo za vzpostavitev ekosistema APIjev v okolju pametnih mest in skupnosti – KumuluzAPI in jo lansirati na globalni trg. Platforma KumuluzAPI pomembno poenostavlja vzpostavitev API ekonomije na nivoju pametnih mest. Vpeljuje decentraliziran marketplace, ki omogoča hitro in učinkovito povezovanje zainteresiranih deležnikov in napredne metode monetizacije APIjev na osnovi uporabe AI. Iz vidika tehničnih inovacij KumuluzAPI vpeljuje city-native APIje, ki omogočajo kontekstno odvisno obnašanje, prilagojen smart-city API prehod s podporo IoT in analitična orodja za spremljanje uporabe APIje na osnovi strojnega učenja. K temu dodaja industrijsko oblikovanje in ekodesign. Gradi na temeljih ekonomije APIjev in vpeljavi novih vrednostnih verig ter omogoča razvoj novih inovativnih rešitev za pametna mesta in skupnosti na različnih področjih, kot so promet, zdravstvo, javna uprava, logistika, itd. Temelji na tehnologijah mikrostoritev, vsebnikov Docker, Kubernetes, edge in fog computinga, decentraliziranih tehnologij, agilnega razvoj ain DevOps.
+                                </p>
+                                <p>
+                                    KumuluzAPI omogoča bolj enostavno povezovanje deležnikov, ki želijo ponuditi APIje, zaradi decentraliziranega načina delovanja pa ne zahteva usklajevanja posameznih deležnikov in omogoča lažje integracije. Prodajno bo umeščen kot eden od elementov digitalne platforme Kumuluz (kumuluz.com), ki je že mednarodno priznana in je že prejela več mednarodnih nagrad.
+                                </p>
+                            </Col>
+                        </Row>
+                        <Row className="justify-content-center mt-4">
+                            <Col xs={12} md={10} lg={8}>
+                                <p className="text-center">NALOŽBO SOFINANCIRATA REPUBLIKA SLOVENIJA IN EVROPSKA UNIJA
+                                    IZ EVROPSKEGA SKLADA ZA REGIONALNI RAZVOJ</p>
+                            </Col>
+                        </Row>
+                        <Row className="justify-content-center mt-1">
+                            <Col xs={8} className="d-flex justify-content-center">
+                                <a className="skladi-url" href="http://www.eu-skladi.si" target="_blank"
+                                    rel="noreferrer noopener">
+                                    www.eu-skladi.si
+                                </a>
+                            </Col>
+                        </Row>
+                    </Container>
+                    
+                    
+                    
+                    
+
+                    {/*<Container fluid={true}>*/}
+                    {/*    <Row className="justify-content-center">*/}
+                    {/*        <Col sm={12} md={6} lg={3} className="sponsor-logo">*/}
+                    {/*            <img src={euLogo} alt="EU"/>*/}
+                    {/*        </Col>*/}
+                    {/*        <Col sm={12} md={6} lg={3} className="sponsor-logo">*/}
+                    {/*            <img src={ministryLogo} alt="RS - Ministry for economical development and technology"/>*/}
+                    {/*        </Col>*/}
+                    {/*    </Row>*/}
+                    {/*</Container>*/}
                     
                     <Container fluid={true} className="project-header">
                         <Row className="align-items-center flex-column">
