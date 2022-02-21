6609e6dda7186c124e6fd3e66e1221bae2507002
Miha Jamsek <miha_jamsek@windowslive.com>
2022-02-21 14:00:20 +0100

    replaced deprecated babel dependencies

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
   extends: ['eslint:recommended', 'plugin:react/recommended'],
-  parser: 'babel-eslint',
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    requireConfigFile: false,
+    babelOptions: {
+      presets: ["@babel/preset-react"]
+    }
+  },
   rules: {
     strict: 0
   },
